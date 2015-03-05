54bbd73927585c3535fce59601e0eda1c6ede20e
KraHager <KraHager@outlook.com>
2015-03-05 16:30:32 +0100

    Innførte metodene main og baklengs

--- Oppgave3a/src/SnuTekst.java
+++ Oppgave3a/src/SnuTekst.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
+
 
 public class SnuTekst {
-
+	static Scanner s = new Scanner(System.in);
+	public static void main(String[] args){
+		System.out.println("Skriv inn en Streng og trykk enter: ");
+		String input = s.nextLine();
+		System.out.print("Baklengs Streng: ");
+		baklengs(input);
+	}
+	
+	public static void baklengs(String tekst){
+		System.out.print(tekst.charAt(tekst.length()-1));
+		if (tekst.length() > 1)
+			baklengs(tekst.substring(0, tekst.length()-1));
+		else
+			System.out.println();
+	}
 }

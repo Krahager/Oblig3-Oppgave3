8cd623f212d0aaae85335f051b0248e8533e2fc6
KraHager <KraHager@outlook.com>
2015-03-05 17:43:59 +0100

    La til en integeger som holder styr på antall forflytninger

--- TowerOfHanoi/src/towerOfHanoi/TowerOfHanoi.java
+++ TowerOfHanoi/src/towerOfHanoi/TowerOfHanoi.java
@@ -3,21 +3,25 @@ package towerOfHanoi;
 import java.util.Scanner;
 
 public class TowerOfHanoi {
+	private static int count;
 	/**Hovedmetode */
 	public static void main(String[] args){
 		//Lager en Scanner
 		Scanner input = new Scanner(System.in);
 		System.out.print("Skriv inn antall disker: ");
 		int n = input.nextInt();
+		input.close();
 		
 		//Find the solution recursively
 		System.out.println("The moves are:");
 		moveDisks(n, 'A', 'B', 'C');
+		System.out.println("\nFerdig flyttet! Antall forflytninger: " + count);
 	}
 
 	/**Metoden for å finne løsningen for å flytte n disker fra fromTower til toTower med auxTower
 	 * som hjelpetårn*/
 	public static void moveDisks(int n, char fromTower, char toTower, char auxTower){
+		count++;
 		if (n==1) //Stoppkrav
 			System.out.println("Move disk " + n + " from " + fromTower + " to " + toTower);
 		else{
